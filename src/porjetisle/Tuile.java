0a78d86c480c11fb22f5cf33fe8ecdda12897c3b
bassetlu <bassetlu@pc-dg-025-13.iut2.upmf-grenoble.fr>
2018-06-04 11:40:01 +0200

    Corps de la classe tuile

--- src/porjetisle/Tuile.java
+++ src/porjetisle/Tuile.java
@@ -5,10 +5,68 @@
  */
 package porjetisle;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author bassetlu
  */
 public class Tuile {
-    
+
+    public enum EtatTuile {
+        SECHE,
+        INONDEE,
+        COULEE
+    }
+
+    public enum NomTuile {
+        //// A ajouter tous noms des tuiles
+    }
+
+    private EtatTuile etat;
+    private NomTuile nomTuile;
+
+    // Associations
+    private Carte_Inond carte;
+    private ArrayList<Aventurier> joueurs;
+    private Position position;
+
+    // METHODES
+    public Tuile(EtatTuile etat, NomTuile nomTuile, Carte_Inond carte, ArrayList<Aventurier> joueurs, Position position) {
+        this.etat = etat;
+        this.nomTuile = nomTuile;
+        this.position = position;
+    }
+
+    public void addJoueur(Joueur joueur) {
+        ////
+    }
+
+    public void supprJoueur(Joueur joueur) {
+        ////
+    }
+
+    public void majEtat() {
+        ////
+    }
+
+    public EtatTuile getEtat() {
+        return etat;
+    }
+
+    public NomTuile getNomTuile() {
+        return nomTuile;
+    }
+
+    public ArrayList<Aventurier> getJoueurs() {
+        return joueurs;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void verifCoulee() {
+        ////
+    }
 }

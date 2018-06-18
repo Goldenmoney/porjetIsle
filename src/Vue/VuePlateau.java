b37e6bd3abf5f07dda22b87e1b25e0b8431aa879
ghazalie <ghazalie@PC-DG-021-09.ntiut2gre.iut2.upmf-grenoble.fr>
2018-06-18 15:40:31 +0200

    ajout grille ihm

--- src/Vue/VuePlateau.java
+++ src/Vue/VuePlateau.java
@@ -7,51 +7,53 @@ package Vue;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class VuePlateau extends JPanel {
-  private JButton buttons[] = new JButton[36];
+
+    private JButton buttons[] = new JButton[36];
+
     public VuePlateau() {
         this.setLayout(new BorderLayout());
-       JPanel panelBody = new JPanel(new BorderLayout());
+        JPanel panelBody = new JPanel(new BorderLayout());
 
-        JPanel panelJeu = new JPanel(new GridLayout(6,6));
-        
-        for(int i = 0; i < 36; i++) {
+        JPanel panelJeu = new JPanel(new GridLayout(6, 6));
+
+        for (int i = 0; i < 36; i++) {
             JButton button = new JButton();
-            
-            final int numCase = i;
-            if (i==0){
-                button.setEnabled(false);
-            }else if(i==1){
-               button.setEnabled(false); 
-            }else if(i==4){
-                button.setEnabled(false);  
-            }else if(i==5){
-                button.setEnabled(false);    
-            }else if(i==6){
-                button.setEnabled(false); 
-            }else if(i==11){
-                button.setEnabled(false); 
-            }else if(i==24){
-                button.setEnabled(false); 
-            }else if(i==29){
-                button.setEnabled(false); 
-            }else if(i==30){
-                button.setEnabled(false); 
-            }else if(i==31){
-                button.setEnabled(false); 
-            }else if(i==34){
-                button.setEnabled(false); 
-            }else if(i==35){
-                button.setEnabled(false); 
+
+           
+            if (i == 0) {
+                button.setVisible(false);
+
+            } else if (i == 1) {
+                button.setVisible(false);
+            } else if (i == 4) {
+               button.setVisible(false);
+            } else if (i == 5) {
+                button.setVisible(false);
+            } else if (i == 6) {
+               button.setVisible(false);
+            } else if (i == 11) {
+                button.setVisible(false);
+            } else if (i == 24) {
+                button.setVisible(false);
+            } else if (i == 29) {
+                button.setVisible(false);
+            } else if (i == 30) {
+                button.setVisible(false);
+            } else if (i == 31) {
+                button.setVisible(false);
+            } else if (i == 34) {
+               button.setVisible(false);
+            } else if (i == 35) {
+               button.setVisible(false);
             }
-            
-            
-            
-            
+
             buttons[i] = button;
+            // final int numCase = i;
 //            button.addActionListener((ActionEvent e) -> {
 //                Message m = new Message();
 //                m.type = TypesMessages.JOUER_COUP;
@@ -59,17 +61,13 @@ public class VuePlateau extends JPanel {
 //                
 //                notifierObservateur(m);
             //}};
-        
-         
+
             panelJeu.add(button);
         }
-            
-           panelBody.add(panelJeu, BorderLayout.CENTER);
-            this.add(panelBody);  
-        }
-      
 
-    
+        panelBody.add(panelJeu, BorderLayout.CENTER);
+        this.add(panelBody);
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();

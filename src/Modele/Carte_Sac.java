6271ebc6b99154fd39a0c1cf5b5c91b24d666112
pontalc <corentin.pontal@laposte.net>
2018-06-19 15:09:15 +0200

    update sur les cartes

---
+++ src/Modele/Carte_Sac.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+import Util.Utils;
+import Util.Utils.TypeCarteTresor;
+import static Util.Utils.TypeCarteTresor.SAC_DE_SABLE;
+
+/**
+ *
+ * @author pontalc
+ */
+public class Carte_Sac extends Carte_Tirage_Tresor {
+    
+    public Carte_Sac(TypeCarteTresor type) {
+        super(SAC_DE_SABLE);
+    }
+    
+}

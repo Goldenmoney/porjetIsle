b726b12c46ebb89dc1f3a52f9e65c7343b111ab8
Elias l'informaticien <ghazalie@pc-dg-037-04.iut2.upmf-grenoble.fr>
2018-06-19 14:30:17 +0200

    modif grille controleur

--- src/Vue/VuePlateau.java
+++ src/Vue/VuePlateau.java
@@ -19,13 +19,16 @@ import Util.Utils.*;
 public class VuePlateau extends JPanel {
 
     private ArrayList<JButton> buttons;
-    private ArrayList<Tuile> tuiles;
+   // private ArrayList<Tuile> tuiles;
+    private Controleur controleur;//passer parametres
 
     public VuePlateau() {
         this.setLayout(new BorderLayout());
         buttons = new ArrayList<>();
-        tuiles = new ArrayList<>();
-        Grille g = new Grille(tuiles);
+     //   tuiles = new ArrayList<>();
+        controleur=new Controleur();//suppr
+        controleur.initGrille(); // suppr 
+        Grille g= controleur.getGrille();
 
         JPanel panelBody = new JPanel(new BorderLayout());
 

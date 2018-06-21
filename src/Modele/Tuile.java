84d7618794aa4a0e7cce196aa1a42438a9054ddc
pontalc <corentin.pontal@laposte.net>
2018-06-21 10:56:59 +0200

    paufinage de la classe tuille

--- src/Modele/Tuile.java
+++ src/Modele/Tuile.java
@@ -5,68 +5,33 @@
  */
 package Modele;
 
-import Util.Utils;
 import java.util.ArrayList;
-import Util.Utils.EtatTuile;
-import Util.Utils.NomTuile;
-import Util.Utils.Pion;
 import Util.Utils.*;
 /**
  *
  * @author bassetlu
  */
 public class Tuile {
-
+    //attributs
     private EtatTuile etat;
     private final NomTuile nomTuile;
     private int posX;
     private int posY;
     private TypeTresor typeTresor;
-
     // Associations
     private Carte_Inond carte;
     private ArrayList<Aventurier> joueurs;
     private Carte_Tresor tresor;
     
-    // METHODES
-  
-public Tuile(EtatTuile etat, NomTuile nomTuile) {
+    // Constructeur
+    public Tuile(EtatTuile etat, NomTuile nomTuile) {
         this.etat = etat;
         this.nomTuile = nomTuile;
         this.joueurs = new ArrayList<>();
     }
     
-//2 constructeurs inutiles
-   /* public Tuile(EtatTuile etat, NomTuile nomTuile, Carte_Inond carte, ArrayList<Aventurier> joueurs, Carte_Tresor tresor) {
-        this.etat = etat;
-        this.nomTuile = nomTuile;
-        this.carte = carte;
-        this.joueurs = joueurs;
-        this.tresor = tresor;
-    }
-    
-     public Tuile(EtatTuile etat, NomTuile nomTuile, Carte_Inond carte) {
-        this.etat = etat;
-        this.nomTuile = nomTuile;
-        this.carte = carte;
-    }*/
-
-    public void setTypeTresor(TypeTresor typeTresor) {
-        this.typeTresor = typeTresor;
-    }
-
-    public TypeTresor getTypeTresor() {
-        return typeTresor;
-    }
-
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-
-    public void setPosY(int posY) {
-        this.posY = posY;
-    }
-
+    //methodes
+    //getter
     public int getPosX() {
         return posX;
     }
@@ -74,20 +39,7 @@ public Tuile(EtatTuile etat, NomTuile nomTuile) {
     public int getPosY() {
         return posY;
     }
-
-    public void addJoueur(Aventurier aventurier) {
-        this.joueurs.add(aventurier);
-    }
-
-    public void supprJoueur(Aventurier aventurier) {
-       this.joueurs.remove(aventurier);
-    }
-
-    //ou setEtat()
-    public void majEtat(EtatTuile etatTuile) {
-        this.etat=etatTuile;
-    }
-
+    
     public EtatTuile getEtat() {
         return etat;
     }
@@ -95,11 +47,45 @@ public Tuile(EtatTuile etat, NomTuile nomTuile) {
     public NomTuile getNomTuile() {
         return nomTuile;
     }
-
+    
     public ArrayList<Aventurier> getJoueurs() {
         return joueurs;
     }
+    
+    public TypeTresor getTypeTresor() {
+        return typeTresor;
+    }
+    
+    //setter
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
 
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+    
+    //ou setEtat()
+    public void majEtat(EtatTuile etatTuile) {
+        this.etat=etatTuile;
+    }
+    
+    public void setTypeTresor(TypeTresor typeTresor) {
+        this.typeTresor = typeTresor;
+    }
+
+    public void setCarte_Inond(Carte_Inond carte) {
+        this.carte = carte;
+    }
+    
+    public void addJoueur(Aventurier aventurier) {
+        this.joueurs.add(aventurier);
+    }
+
+    public void supprJoueur(Aventurier aventurier) {
+       this.joueurs.remove(aventurier);
+    }
+    
     public boolean verifCoulee() {
         return getEtat()==EtatTuile.COULEE;
     }
@@ -107,10 +93,4 @@ public Tuile(EtatTuile etat, NomTuile nomTuile) {
     public boolean verifInondee() {
         return getEtat()==EtatTuile.INONDEE;
     }
-
-    public void setCarte_Inond(Carte_Inond carte) {
-        this.carte = carte;
-    }
-    
-    
 }
